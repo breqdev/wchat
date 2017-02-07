47a91a549f6376aacf6ca2413e69d9e6ea1cc6a5
Wesley Chalmers <wesleyjchalmers@gmail.com>
2017-02-07 15:43:26 -0500

    Add custom password options and a better README

--- weschat.py
+++ weschat.py
@@ -7,27 +7,43 @@ window.title("WesChat")
 wc = None
 
 def connect():
-    global wc, hostEntry
-    wc = WesChat(hostEntry.get(), "Ni")
+    global wc, hostEntry, passEntry
+    wc = WesChat(hostEntry.get(), passEntry.get())
 
 def post(blah=None):
     global wc, postEntry
     wc.post(postEntry.get())
     postEntry.delete(0, tkinter.END)
 
-connectBtn = tkinter.Button(window, text="Connect", command=connect)
-postBtn = tkinter.Button(window, text="Post", command=post)
-hostEntry = tkinter.Entry(window)
-postEntry = tkinter.Entry(window)
+connectBtn = tkinter.Button(window, text="Connect", command=connect,
+                            font=("Courier New", 12))
+postBtn = tkinter.Button(window, text="Post", command=post,
+                         font=("Courier New", 12))
+
+hostLabel = tkinter.Label(window, text="Server IP:", font=("Courier New", 12))
+passLabel = tkinter.Label(window, text="Password:", font=("Courier New", 12))
+
+hostEntry = tkinter.Entry(window, font=("Courier New", 12))
+passEntry = tkinter.Entry(window, font=("Courier New", 12), show="*")
+postEntry = tkinter.Entry(window, font=("Courier New", 12))
+
 msgLabel = tkinter.Text(window, font=("Courier New", 12), wrap=tkinter.WORD)
-connectBtn.grid(row=0, column=1, sticky=tkinter.E+tkinter.N)
-postBtn.grid(row=2, column=1, sticky=tkinter.E)
-hostEntry.grid(row=0, column=0, sticky=tkinter.W+tkinter.E+tkinter.N)
+
+connectBtn.grid(row=0, column=2, sticky=tkinter.E+tkinter.N)
+postBtn.grid(row=3, column=2, sticky=tkinter.E, rowspan=2)
+
+hostLabel.grid(row=0, column=0, sticky=tkinter.W+tkinter.N)
+passLabel.grid(row=1, column=0, sticky=tkinter.W)
+
+hostEntry.grid(row=0, column=1, sticky=tkinter.W+tkinter.E+tkinter.N)
+passEntry.grid(row=1, column=1, sticky=tkinter.W+tkinter.E+tkinter.N)
 postEntry.bind("<Return>", post)
-postEntry.grid(row=2, column=0, sticky=tkinter.W+tkinter.E)
-msgLabel.grid(row=1, column=0, columnspan=2, sticky=tkinter.W)
+postEntry.grid(row=3, column=0, columnspan=2, sticky=tkinter.W+tkinter.E)
+
+msgLabel.grid(row=2, column=0, columnspan=3, sticky=tkinter.W)
+
 window.rowconfigure(0, weight=1)
-window.columnconfigure(0, weight=1)
+window.columnconfigure(1, weight=1)
 
 while True:
     window.update()
