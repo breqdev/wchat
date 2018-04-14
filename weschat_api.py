bb388789492f03a554f27403261efb2699f66cc9
Wesley Chalmers <wesleyjchalmers@gmail.com>
2018-04-13 23:48:50 -0400

    add automatic username detection based on hostname

--- weschat_api.py
+++ weschat_api.py
@@ -1,13 +1,16 @@
 import socket
 from select import select
+import time
 
 class WesChat:
     def __init__(self, host, handshake, port=8888, name=None):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((host, port))
         self.post(handshake)
-        if name != None:
-            self.post("/uname "+name)
+        if name == None:
+            name = socket.gethostname()
+        time.sleep(1)
+        self.post("/uname "+name)
     def post(self, message):
         self.s.send(message.encode())
     def get_messages(self):
